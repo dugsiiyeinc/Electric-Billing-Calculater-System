173155765766d10b7b27fe516d211d44a4af03d6
shiinedev <shiinecadde17@gmail.com>
2025-02-12 15:21:19 +0300

    added start  setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Currency Exchange & user Management system
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+  <>
+  <h1>Currency Exchange & user Management system</h1>
+
+  </>
+  )
+}
+
+export default App
