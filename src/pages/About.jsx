e177e3b7846f5ecdac4a0fb5351f462d1b909832
shiinedev <shiinecadde17@gmail.com>
2025-02-15 17:20:16 +0300

    added navbar in about page ✅

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const About = () => {
   return (
-    <div>
+    <div  className="min-h-screen bg-gray-100 ">
+        <Navbar />
       
     </div>
   )
