b81b153ebf55ce67dde24dc2cdff3460625733c6
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-02-15 12:04:18 +0300

    ➕ Added a state for mobile drawer toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { X, Menu } from "lucide-react";
 
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
     
 
     return (
