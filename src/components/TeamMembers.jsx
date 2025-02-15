9405a3dfa32d0a3942efeecafe9d03d382dd9f68
shiinedev <shiinecadde17@gmail.com>
2025-02-15 17:52:47 +0300

    📄 add :  added team mebers data m

---
+++ src/components/TeamMembers.jsx
@@ -0,0 +1,28 @@
+// TeamSection.jsx
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+
+
+
+const TeamMembers = () => {
+    const teamMembersData = [
+        {
+          name: 'Faarax Abdullahi',
+          role: 'Frontend Developer',
+          imageUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+          name: 'Shiine dev',
+          role: 'Frontend Developer',
+          imageUrl: 'https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        
+      ];
+  return (
+    <div className="mt-12 ">
+      
+    </div>
+  );
+};
+
+export default TeamMembers;
