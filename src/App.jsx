a74bb407018f03b7c867377fb3b5487221171d26
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-02-15 13:20:26 +0300

    ✅feat: added and completed Features section with dynamic feature cards, icons, titles, and descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Feature from "./components/Feature";
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Feature/>
     </div>
   );
 };
