ef112f2d25fc50f15c8d566805953097784b550e
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-02-16 13:52:27 +0300

    🛠 refactor(router): clean up and improve readability of router setup in the App and used in the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-// In your App component or wherever you use the routes
 import React from "react";
-import { useRoutes } from "react-router-dom";
-import Home from "../src/pages/Home";
-import About from "../src/components/About";
-import Navbar from "./components/Navbar";
-import BillingCalculater from "./components/BillingCalculater";
-const App = () => {
-  const routes = useRoutes([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/billing-calculater",
-      element: <BillingCalculater />,
-    },
-  ]);
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
+import About from "./components/About"; 
+import BillingCalculater from "./components/BillingCalculater"; 
 
-  return routes;
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/billing-calculater" element={<BillingCalculater />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
