ff8516c0bb7f8fbf5e30b37146e0802d20163d26
shiinedev <shiinecadde17@gmail.com>
2025-02-17 18:40:28 +0300

    (add): finish billing item ui and data ✅

--- src/components/billing History/BillingItem.jsx
+++ src/components/billing History/BillingItem.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const BillingItem = () => {
+const BillingItem = ({row}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <tr  className="even:bg-gray-100 text-center">
+      <td className="border border-gray-300 text-sm  p-2">{row.id}</td>
+      <td className="border border-gray-300 text-sm  p-2">{row.name}</td>
+      <td className="border border-gray-300 text-sm  p-2">{row.bill}</td>
+      <td className="border border-gray-300 text-sm  p-2">{row.rate}</td>
+      <td className="border border-gray-300 text-sm  p-2">{row.lastMonth}</td>
+      <td className="border border-gray-300 text-sm  p-2">
+        {row.currentMonth}
+      </td>
+      <td
+        className={`border border-gray-300 text-sm  p-2 ${
+          row.profit < 0 ? "text-red-500" : "text-green-500"
+        }`}
+      >
+        {row.profit}
+      </td>
+    </tr>
+  );
+};
 
-export default BillingItem
+export default BillingItem;
