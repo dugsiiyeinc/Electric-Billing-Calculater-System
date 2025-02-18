aebd97f06f618503b24ca0044593c4d8eff9aa4e
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-02-18 13:28:10 +0300

    🔧 fixed the diverging conflict which occure due to movement of initialState object to another file

--- src/components/profile Management/ProfileContext.jsx
+++ src/components/profile Management/ProfileContext.jsx
@@ -2,23 +2,8 @@ import React from "react";
 import { useReducer } from "react";
 import { useContext } from "react";
 import { profileContext } from "./ProfileContext";
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 import { initialState } from "./initialState";
-
-
-=======
->>>>>>> e846ba4 (movied the initial state into a different file)
-
-export const initialState = {
-    user:[],
-}
-<<<<<<< HEAD
-=======
->>>>>>> 5cdd01514a9d21486e103f8e93e2c78ac30f9e00
->>>>>>> e846ba4 (movied the initial state into a different file)
-
 
 
 export const useProfile = () => useContext(profileContext);
