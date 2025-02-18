859fd0f5aaa49856167b1d5c0035da7b8d675b17
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-02-18 13:42:04 +0300

    💡 feat: implement profile reducers for user management
    - Added ADD_USER case to append a new user to the users array.
    - Added UPDATE_USER case to update an existing user by ID.
    - Added DELETE_USER case to remove a user by ID.
    - Default case returns the current state for unknown actions.

--- src/components/profile Management/ProfileContext.jsx
+++ src/components/profile Management/ProfileContext.jsx
@@ -4,12 +4,13 @@ import { useContext } from "react";
 import { profileContext } from "./ProfileContext";
 
 import { initialState } from "./initialState";
+import { profileReducers } from "../reducers/ProfileReducer";
 
 
 export const useProfile = () => useContext(profileContext);
 
 export const ProfileProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(profileReducer, initialState);
+  const [state, dispatch] = useReducer(profileReducers, initialState);
 
   const value = { state, dispatch };
 
