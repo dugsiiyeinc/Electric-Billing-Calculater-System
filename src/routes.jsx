c7362cdcc40ce47ac52c5fde767b1f7724c0ce73
shiinedev <shiinecadde17@gmail.com>
2025-02-25 14:09:16 +0300

    (change

---
+++ src/routes.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/Home";
+import About from "./pages/About";
+import BillingApp from "./components/billing History/BillingApp";
+import BillingCalculator from "./components/BillingCalculator";
+import Login from "./pages/Login";
+import ProfilePage from "./components/profile Management/ProfilePage";
+
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App />,
+        children:[
+            {
+                index:true,
+                element:<HomePage />
+            },
+            {
+                path:"/about",
+                element:<About />
+
+            },
+            {
+                path:"/savedData",
+                element:<BillingApp />
+
+            },
+            {
+                path:"/billingCalculator",
+                element:<BillingCalculator />
+
+            },
+            {
+                path:"/login",
+                element:<Login />
+
+            },
+            {
+                path:"/profile",
+                element:<ProfilePage />
+
+            }
+        ]
+
+    }
+]);
+
+export default router;
