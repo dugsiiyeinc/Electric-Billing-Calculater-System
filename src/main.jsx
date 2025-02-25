1ae33709c4cf26e035648e458eb777537ee64b0a
shiinedev <shiinecadde17@gmail.com>
2025-02-25 16:01:42 +0300

    (fix): removing Authprovider in main.js🐛

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import {  RouterProvider,  } from "react-router-dom";
 import router from "./routes.jsx";
 import "./index.css"
-import AuthProvider from "./contexts/AuthContext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
