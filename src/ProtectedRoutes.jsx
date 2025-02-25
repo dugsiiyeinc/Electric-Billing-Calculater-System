bd908d58f02301ecf852bc9e9aefe4cb959e9200
shiinedev <shiinecadde17@gmail.com>
2025-02-25 16:54:11 +0300

    (feat):show toast massge when user goes protectedRoutes✅

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react'
+import toast from 'react-hot-toast';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoutes = ({element}) => {
     const onlineUser = JSON.parse(localStorage.getItem("onlineUser")) || null;
 
     if(!onlineUser){
+        toast.error("if you want access savedData first Login !");
         return <Navigate to="/login" replace />
        
     }
-  return element;
+
+   return element;
   
 }
 
